bd784bacbcee17f10d59a25f6d0953c2281824db
hellojukay <hellojukay@163.com>
2021-05-05 21:39:42 +0800

    Update main.go

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("去读日期信息失败了", err)
 	}
-	t, err := time.Parse(`"2006:01:02 15:04:05"`, v.String())
+	t, err := time.Parse(`"2006:01:02"`, v.String())
 	if err != nil {
 		log.Fatalf("无法解析日期 %s\n", v.String())
 	}
