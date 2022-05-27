dcc8a3714012aca8be5537d25fc9b1a622d6f62d
李聪 <licong@qianxin.com>
2022-05-27 11:49:02 +0800

    添加处理时间统计

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	_ "embed"
@@ -27,6 +28,7 @@ var (
 	fontBuffer []byte
 	output     string
 	fontSize   = 34
+	wg         sync.WaitGroup
 )
 
 func init() {
@@ -54,6 +56,7 @@ func init() {
 	}
 }
 func main() {
+	var begin = time.Now()
 	fname := file
 
 	fh, err := os.Open(fname)
@@ -66,7 +69,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	log.Printf("[%10s] read file finish\n", time.Now().Sub(begin))
 	// 去读日期信息
 	v, err := x.Get("DateTimeOriginal")
 	if err != nil {
@@ -76,24 +79,39 @@ func main() {
 	if err != nil {
 		log.Fatalf("照片%s无法解析日期 %s\n", fname, v.String())
 	}
+	log.Printf("[%10s] read time from image finish\n", time.Now().Sub(begin))
+
 	var txt = t.Format("2006-01-02 15:04:05")
 	font, err := freetype.ParseFont(fontBuffer)
 	if err != nil {
 		log.Fatalf("解析字体文件 %s 文件失败，这可能不是一个合法的字体文件,%s\n", fontFile, err)
 	}
 	fh.Seek(0, 0)
+	log.Printf("[%10s] encode image begin\n", time.Now().Sub(begin))
+
 	jpgimg, err := jpeg.Decode(fh)
 	if err != nil {
 		log.Fatalf("解析图片文件失败，请检查文件是否合法 %s\n", err)
 	}
+	log.Printf("[%10s] encode image finish\n", time.Now().Sub(begin))
 
 	img := image.NewNRGBA(jpgimg.Bounds())
+	var maxY = img.Bounds().Dy()
+	var maxX = img.Bounds().Dx()
+	log.Printf("[%10s] set color begin\n", time.Now().Sub(begin))
 
-	for y := 0; y < img.Bounds().Dy(); y++ {
-		for x := 0; x < img.Bounds().Dx(); x++ {
-			img.Set(x, y, jpgimg.At(x, y))
+	for y := 0; y < maxY; y++ {
+		for x := 0; x < maxX; x++ {
+			wg.Add(1)
+			go func(x, y int) {
+				defer wg.Done()
+				img.Set(x, y, jpgimg.At(x, y))
+			}(x, y)
 		}
 	}
+	wg.Wait()
+	log.Printf("[%10s] draw text begin\n", time.Now().Sub(begin))
+
 	f := freetype.NewContext()
 	f.SetFont(font)
 	f.SetDPI(400)
@@ -106,6 +124,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("写入水印失败 %s\n", err.Error())
 	}
+	log.Printf("[%10s] draw text finish\n", time.Now().Sub(begin))
+
 	// 保存到新的文件中
 	newfile, _ := os.Create(output)
 	defer newfile.Close()
@@ -113,8 +133,12 @@ func main() {
 	err = jpeg.Encode(newfile, img, &jpeg.Options{
 		Quality: 100,
 	})
+	log.Printf("[%10s] encode to file finished\n", time.Now().Sub(begin))
+
 	if err != nil {
 		log.Fatalf("保存水印文件%s失败%s", output, err)
 		os.Exit(1)
 	}
+	log.Printf("[%10s] save to file finish\n", time.Now().Sub(begin))
+
 }
